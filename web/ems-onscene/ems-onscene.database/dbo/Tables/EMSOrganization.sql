cec7944f1e3c3f0380752ec0bcc21d743bc967b0
jae hun, jung <hanhead@gmail.com>
2023-08-26 19:51:30 -0700

    add emsorganization

---
+++ web/ems-onscene/ems-onscene.database/dbo/Tables/EMSOrganization.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EMSOrganization] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [OrganizationName] NVARCHAR (100) NOT NULL,
+    [LogoImageUrl]     NVARCHAR (MAX) NULL,
+    [HomepageImageUrl] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_EMSOrganization] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
