36f08e4634ea2c986141d984b99a1fbc0e702ec7
jae hun, jung <hanhead@gmail.com>
2023-08-27 10:15:59 -0700

    add bagcheck table

---
+++ web/ems-onscene/ems-onscene.database/dbo/Tables/EMSBagCheck.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EMSBagCheck] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [BagCheckTitle] INT            NOT NULL,
+    [Detail]        NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_EMSBagCheck] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_EMSBagCheck_EMSBagCheckDetail] FOREIGN KEY ([BagCheckTitle]) REFERENCES [dbo].[EMSBagCheckDetail] ([Id])
+);
+
---
+++ web/ems-onscene/ems-onscene.database/dbo/Tables/EMSBagCheckDetail.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[EMSBagCheckDetail] (
+    [Id]            INT IDENTITY (1, 1) NOT NULL,
+    [EMSBagCheckId] INT NULL,
+    CONSTRAINT [PK_EMSBagCheckDetail] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
