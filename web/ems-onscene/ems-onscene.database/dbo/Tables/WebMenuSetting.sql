4125ccd30cd245936f41e0b9fbde7a508a9c173a
jae hun, jung <hanhead@gmail.com>
2023-09-09 19:22:25 -0700

    add web menu table

---
+++ web/ems-onscene/ems-onscene.database/dbo/Tables/WebMenuSetting.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[WebMenuSetting] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [ParentId] INT           NULL,
+    [MenuName] NVARCHAR (50) NOT NULL,
+    [URL]      NCHAR (10)    NULL,
+    [Sorting]  INT           NULL,
+    CONSTRAINT [PK_WebMenuSetting] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_WebMenuSetting_WebMenuSetting] FOREIGN KEY ([ParentId]) REFERENCES [dbo].[WebMenuSetting] ([Id])
+);
+
