719d125aaa7166dcaa0ca219c69bb0e4fc4cddd5
james <hanhead@gmail.com>
2023-09-12 18:23:19 -0700

    pagination

--- web/ems-onscene/ems-onscene/Controllers/InfoLookupController.cs
+++ web/ems-onscene/ems-onscene/Controllers/InfoLookupController.cs
@@ -22,14 +22,19 @@ namespace acemsoncall.web.Controllers
             base.Dispose(disposing);
         }
         // GET: InfoLookup
-        public ActionResult Index(string s)
+        public ActionResult Index(string s, int p = 1, int ps = 5)
         {
             var eMSContents = db.EMSContents.Where(e => e.contenttype == "Information");
             if (string.IsNullOrEmpty(s) == false)
             {
                 eMSContents = eMSContents.Where(m => m.title.IndexOf(s) >= 0);
             }
-            eMSContents = eMSContents.OrderByDescending(e => e.id).Include(e => e.AspNetUser).Include(e => e.AspNetUser1);
+            int recordCount = eMSContents.Count();
+            int pageCount = (recordCount / ps) + (recordCount % ps > 0 ? 1 : 0);
+            ViewBag.currentPage = p;
+            ViewBag.recordCount = recordCount;
+            ViewBag.pageCount = pageCount;
+            eMSContents = eMSContents.OrderByDescending(e => e.id).Include(e => e.AspNetUser).Include(e => e.AspNetUser1).Skip((p - 1) * ps).Take(ps);
             return View(eMSContents.ToList());
         }
     }
